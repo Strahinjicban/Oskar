6830be6e5a0c621a146fb2fe66492428b7d4c1bc
Strahinja <you@example.com>
2023-01-20 18:33:00 +0100

    strebanje

---
+++ vezba.py
@@ -0,0 +1,22 @@
+ime ="Strahinjia"
+prezime = "Milojevic"
+godiste = 1989
+mesto = "Kraguejvac"
+automobil = "Wolsvagen" 
+prosecna_brzina = 65
+
+print(ime) 
+print (prezime)
+print (mesto)
+if prosecna_brzina==100:
+    print("baba vozac")
+else:
+    print("majstor") 
+    
+if godiste < 1999:
+    print ("mator")
+elif godiste==1932:
+    print("odlican")
+
+else:
+    print("mladic")
